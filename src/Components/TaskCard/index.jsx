bbbaf31335f1b3e942994dd6909efc848a77164a
Victor-OT <crowbreaker96@hotmail.com>
2024-07-22 13:23:51 -0600

    Se realizó la lógica de marcar o desmarcar un task

--- src/Components/TaskCard/index.jsx
+++ src/Components/TaskCard/index.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react'
 import './TaskCard.css'
 
 function TaskCard (props) {
+    const [isTaskComplete, setIsTaskComplete] = useState("Incomplete-task")
+    const setTaskStatus = () => {
+        isTaskComplete === "incomplete-task" ? setIsTaskComplete("completed-task")
+        : setIsTaskComplete("incomplete-task")
+    }
+
     return (
-        <div className="task-card-container">
+        <div className={`task-card-container ${isTaskComplete}`}>
             <img 
                 className="close-icon"
                 src="/icon-close.svg" alt="close" />
             <p>{props.task}</p>
             <img
                 className="check-icon" 
-                src="/icon-check.svg" alt="check" />
+                src="/icon-check.svg" alt="check" 
+                onClick={() => setTaskStatus()}/>
         </div>
     )
 }
